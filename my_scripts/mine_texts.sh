88f7bc9c22cf3e4d8b8dbb594230c6419f81b0d0
Maria <marialomeli@fb.com>
2023-06-21 14:29:56 -0700

    bash scripts

---
+++ my_scripts/mine_texts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:8
+#SBATCH --time=48:00:00
+#SBATCH --job-name=mine_texts
+#SBATCH --output=/checkpoint/marialomeli/jobs/%A
+#SBATCH --signal=USR1@140
+#SBATCH --open-mode=append
+#SBATCH --partition=devlab
+
+export LASER="${HOME}/LASER"
+CODE_SIZE=64
+K=120
+TARGET_LNG="en"
+SRC_LNG="fr"
+OUTPUT
+srun python3 source/mine_bitexts.py /private/home/marialomeli/LASER/tasks/bucc/embed/bucc2018.${SRC_LNG}-${TARGET_LNG}.train.txt.${SRC_LNG} \
+ /private/home/marialomeli/LASER/tasks/bucc/embed/bucc2018.${SRC_LNG}-${TARGET_LNG}.train.txt.${TARGET_LNG} \
+--src-lang de --trg-lang en --src-embeddings  /private/home/marialomeli/LASER/tasks/bucc/embed/sonar_embeds/encf.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.${SRC_LNG} \
+--trg-embeddings /private/home/marialomeli/LASER/tasks/bucc/embed/sonar_embeds/encf.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.${TARGET_LNG} --mode mine --retrieval max \
+--margin ratio -k ${K} --code_size ${CODE_SIZE} --output /private/home/marialomeli/LASER/tasks/bucc/embed/sonar${CODE_SIZE}.k${K}.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.candidates.tsv --verbose --fp16 --unify 
---
+++ my_scripts/uncompressed_mine_texts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:8
+#SBATCH --time=48:00:00
+#SBATCH --job-name=mine_texts
+#SBATCH --output=/checkpoint/marialomeli/jobs/%A
+#SBATCH --signal=USR1@140
+#SBATCH --open-mode=append
+#SBATCH --partition=devlab
+
+export LASER="${HOME}/LASER"
+K=120
+TARGET_LNG="en"
+SRC_LNG="de"
+
+srun python3 source/mine_bitexts.py /private/home/marialomeli/LASER/tasks/bucc/embed/bucc2018.${SRC_LNG}-${TARGET_LNG}.train.txt.${SRC_LNG} \
+ /private/home/marialomeli/LASER/tasks/bucc/embed/bucc2018.${SRC_LNG}-${TARGET_LNG}.train.txt.${TARGET_LNG} \
+--src-lang de --trg-lang en --src-embeddings  /private/home/marialomeli/LASER/tasks/bucc/embed/sonar_embeds/encf.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.${SRC_LNG} \
+--trg-embeddings /private/home/marialomeli/LASER/tasks/bucc/embed/sonar_embeds/encf.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.${TARGET_LNG} --mode mine --retrieval max \
+--margin ratio -k ${K} --output /private/home/marialomeli/LASER/tasks/bucc/embed/sonar.k${K}.bucc2018.${SRC_LNG}-${TARGET_LNG}.train.candidates.tsv --verbose --fp16 --unify  --gpu
